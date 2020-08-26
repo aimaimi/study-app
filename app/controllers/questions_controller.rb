7be038d5467183c640f0cba1937fec97d7688c93
aimaimi <yh1127@hotmail.co.jp>
2020-08-26 15:34:29 +0900

    問題作成機能

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,4 +2,19 @@ class QuestionsController < ApplicationController
   def new
     @question = Question.new
   end
+
+  def create
+    @question = Question.new(question_params)
+    if @question.valid?
+      @question.save
+      redirect_to user_path(current_user)
+    else
+      render :new
+    end
+  end
+
+  private
+  def question_params
+    params.require(:question).permit(:problem_statement, :correct_answer).merge(user_id: current_user.id)
+  end
 end
