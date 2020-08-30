b0544f45c39d5b147b9ac758f66fb5ffd6588799
aimaimi <yh1127@hotmail.co.jp>
2020-08-30 17:21:32 +0900

    現在時刻保存機能

--- app/controllers/time_managements_controller.rb
+++ app/controllers/time_managements_controller.rb
@@ -1,4 +1,12 @@
 class TimeManagementsController < ApplicationController
+  protect_from_forgery with: :null_session
+
   def index
+  end
+
+  def create
+    time = params.keys[0]
+    user_id = params[:user_id]
+    TimeManagement.create(time: time, user_id: user_id)
   end
 end
--- app/javascript/timer.js
+++ app/javascript/timer.js
@@ -1,10 +1,18 @@
 window.addEventListener('load', function(){
   const start = document.getElementById("start")
   const stop = document.getElementById("stop")
+  const reset = document.getElementById("reset")
   const sound = document.getElementById("sound")
-
   let timer1;
-  
+
+  const time1 = new Date();
+  const hour = time1.getHours();
+  const minute = time1.getMinutes();
+  const second = time1.getSeconds();
+  let time2 = hour + ":" + minute + ":" + second;
+
+  const user = document.getElementById("user").innerHTML
+
   start.addEventListener('click', function cntStart(){
     timer1 = setInterval(countDown, 1000);
   })
@@ -13,10 +21,13 @@ window.addEventListener('load', function(){
     clearInterval(timer1);
   })
 
+  reset.addEventListener('click', function(){
+    cntReset();
+  })
+
   let countDown = function(){
     let min = document.getElementById("min").value
     let sec = document.getElementById("sec").value
-
     if((min == "") && (sec == "")){
       alert("時間を設定してください！");
       reset();
@@ -33,18 +44,26 @@ window.addEventListener('load', function(){
 
   function secWrite(int){
     if(int < 0){
-      reset();
+      cntReset();
       sound.play();
       alert("時間です！");
+      studyTime();
     }else{
       document.timer.min.value = Math.floor(int / 60);
       document.timer.sec.value = int % 60;
     }
   }
 
-  function reset(){
+  function cntReset(){
     document.timer.min.value = 0;
     document.timer.sec.value = 0;
     clearInterval(timer1);
+  }
+
+  function studyTime(){
+    const XHR = new XMLHttpRequest();
+    XHR.open("POST", `/users/${user}/time_managements`, true);
+    XHR.setRequestHeader('content-type', 'application/x-www-form-urlencoded;charset=UTF-8');
+    XHR.send(time2);
   }
 })
