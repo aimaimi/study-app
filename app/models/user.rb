e00206af81a70125525a655630fc20b3e1999cb4
aimaimi <yh1127@hotmail.co.jp>
2020-08-31 15:15:13 +0900

    nicknameは一意性がある

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,8 @@ class User < ApplicationRecord
   has_many :answers
   has_many :time_managements
 
+  validates :nickname, {presence: true, uniqueness: true}
+
   def self.search(search)
     if search != ''
       User.where('nickname LIKE(?)', "%#{search}%")
