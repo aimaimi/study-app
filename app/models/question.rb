64c1286f15ac3a9c53d189e8aabfa8586b9d7a54
aimaimi <yh1127@hotmail.co.jp>
2020-08-31 17:29:13 +0900

    Questionモデルのテストコード

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,9 @@
 class Question < ApplicationRecord
   belongs_to :user
   has_many :answers, dependent: :destroy
+
+  with_options presence: true do
+    validates :problem_statement
+    validates :correct_answer
+  end
 end
