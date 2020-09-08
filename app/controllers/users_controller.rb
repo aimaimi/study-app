0ae8d33b0dde1c39ae3ec28d22bb717068112d58
aimaimi <yh1127@hotmail.co.jp>
2020-09-08 19:09:05 +0900

    ユーザーリストへのページネーション実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,11 @@
 class UsersController < ApplicationController
   def index
-    @users = User.all.order('created_at DESC')
+    @users = User.all.order('created_at DESC').page(params[:page]).per(10)
   end
 
   def show
     @user = User.find(params[:id])
-    @times = @user.time_managements.where(user_id: @user.id)
+    @times = @user.time_managements.where(user_id: @user.id).page(params[:page]).per(10)
     @graph = []
     @times.each do |time|
       @graph << [time.date, time.time / 60]
@@ -13,6 +13,6 @@ class UsersController < ApplicationController
   end
 
   def search
-    @users = User.search(params[:keyword]).all.order('created_at DESC')
+    @users = User.search(params[:keyword]).all.order('created_at DESC').page(params[:page]).per(20)
   end
 end
